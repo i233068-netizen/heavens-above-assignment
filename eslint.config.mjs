ed7b312ed33e85f646f9b132e2011a4afe02814b
i233068-netizen <i233068@isb.nu.edu.pk>
2025-10-24 10:43:43 +0500

    Fix import statement in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,10 @@
-import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"], // all JS files recursively
-    extends: ["js/recommended"],
+    extends: ["js/recommended"],   // recommended JS rules
     languageOptions: {
       globals: {
         ...globals.browser,
