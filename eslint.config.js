4979c1dc733f48a37a424f2603139adb4c212012
Ayesha Naveed <i233068@isb.nu.edu.pk>
2025-10-24 08:50:51 +0500

    Eslint added

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,8 @@
 import js from "@eslint/js";
 import globals from "globals";
+import { defineConfig } from "eslint/config";
 
-export default [
-  js.configs.recommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-    },
-    rules: {
-      "no-undef": "error",
-      "no-unused-vars": "warn",
-    },
-  },
-];
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+]);
